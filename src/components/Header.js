0107f7fe8664131e99c4338cb5cad7703a97455d
github <github>
2023-02-17 12:24:16 -0500

    📝 Import from study-react

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { Inter } from "@next/font/google";
+import styles from "./Header.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.anchor}>
+        Index
+      </Link>
+      <Link href="/about" className={styles.anchor}>
+        About
+      </Link>
+    </header>
+  );
+}
